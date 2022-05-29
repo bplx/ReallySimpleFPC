580d600ae6b4a6da9d0bc7e6a4ab8ec0abc21597
bplx <satosummering@protonmail.com>
2022-05-29 18:17:52 -0400

    improve stuff and add sprint and dev textures

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,24 +1,28 @@
 extends KinematicBody
-
+export var Sprint_Speed = 4
+export var Normal_Speed = 2
 export var GRAVITY = 10
-export var SPEED = 10
 export var JUMPFORCE = 10
+
+var SPEED = 10
 var mouse_sensitivity = 0.01 / 2
 
 var velocity = Vector3(0, 0, 0)
 
-func handle_input():
+func handle_input(delta):
 	var input_dir = Vector3(0, 0, 0)
 	
 	if Input.is_action_pressed("Forward"):
-		input_dir += global_transform.basis.z
+		input_dir += global_transform.basis.z * delta
 	if Input.is_action_pressed("Backward"):
-		input_dir += -global_transform.basis.z
+		input_dir += -global_transform.basis.z * delta
 	if Input.is_action_pressed("Left"):
-		input_dir += global_transform.basis.x
+		input_dir += global_transform.basis.x * delta
 	if Input.is_action_pressed("Right"):
-		input_dir += -global_transform.basis.x
-		
+		input_dir += -global_transform.basis.x * delta
+	if Input.is_action_pressed("Sprint"):
+		SPEED = Sprint_Speed * 5
+	
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		input_dir += global_transform.basis.y
 		
@@ -35,17 +39,16 @@ func handle_mouse_movement(ev):
 	$Head.rotation.x = clamp($Head.rotation.x, -1.2, 1.2)
 
 func _physics_process(delta):
-	
-	print(is_on_floor())
-	
+	SPEED = Normal_Speed * 5
+
 	velocity.y -= GRAVITY
 
-	velocity.x += handle_input().x * SPEED
-	velocity.y += handle_input().y * JUMPFORCE
-	velocity.z += handle_input().z * SPEED
+	velocity.x += handle_input(delta).x * SPEED
+	velocity.y += handle_input(delta).y * JUMPFORCE
+	velocity.z += handle_input(delta).z * SPEED
 	
-	velocity.x = velocity.x * 0.9
-	velocity.z = velocity.z * 0.9
+	velocity.x = velocity.x * 0.8
+	velocity.z = velocity.z * 0.8
 	
 	velocity = move_and_slide(velocity, Vector3.UP)
 	
